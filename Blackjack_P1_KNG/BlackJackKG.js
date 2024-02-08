324a90851c48ecfacbb4246f4a1d44730639af2d
Korbin Gillette <korbingillette@Korbins-MacBook-Pro.local>
2024-02-07 20:25:00 -0800

    P1 second commit

--- Blackjack_P1_KNG/BlackJackKG.js
+++ Blackjack_P1_KNG/BlackJackKG.js
@@ -1,5 +1,71 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var readline_sync_1 = require("readline-sync");
-var move = readline_sync_1.default.question("choose your move: (h)it, (s)tay.");
-console.log(move);
+//import readline from 'readline-sync';
+var Game = /** @class */ (function () {
+    function Game() {
+        var deck = new Deck();
+        deck.shuffleDeck(deck.drawdeck);
+        console.log(deck.drawdeck);
+        //this.playerHand
+    }
+    Game.prototype.startGame = function () {
+        //const move = readline.question("choose your move: (h)it, (s)tay.");
+        //console.log(move);
+    };
+    return Game;
+}());
+var Deck = /** @class */ (function () {
+    function Deck() {
+        this.drawdeck = new Array;
+        var cardNum = 2;
+        for (var i = 0; i <= 52; i++) {
+            if (i < 36) {
+                this.drawdeck[i] = cardNum++;
+                if (cardNum == 10) {
+                    cardNum = 2;
+                }
+            }
+            else {
+                this.drawdeck[i++] = "Ace";
+                this.drawdeck[i++] = "Jack";
+                this.drawdeck[i++] = "Queen";
+                this.drawdeck[i] = "King";
+            }
+        }
+        console.log(this.drawdeck);
+    }
+    Deck.prototype.shuffleDeck = function (unshuffled) {
+        for (var i = 0; i <= 52; i++) {
+            var rndNum = Math.floor(Math.random() * 51);
+            var rndOne = unshuffled[i];
+            var rndTwo = unshuffled[rndNum];
+            unshuffled[rndNum] = rndOne;
+            unshuffled[i] = rndTwo;
+        }
+    };
+    Deck.prototype.drawCard = function () {
+        //this.
+        return 0;
+    };
+    return Deck;
+}());
+var Cards = /** @class */ (function () {
+    function Cards(playername) {
+        this.playername = playername;
+        this.hand = [0, 0];
+    }
+    return Cards;
+}());
+var Suits;
+(function (Suits) {
+    Suits[Suits["Hearts"] = 0] = "Hearts";
+    Suits[Suits["Diamonds"] = 1] = "Diamonds";
+    Suits[Suits["Clubs"] = 2] = "Clubs";
+    Suits[Suits["Spades"] = 3] = "Spades";
+})(Suits || (Suits = {}));
+var Values;
+(function (Values) {
+    Values[Values["Ace"] = 11] = "Ace";
+    Values[Values["Jack"] = 10] = "Jack";
+    Values[Values["Queen"] = 10] = "Queen";
+    Values[Values["King"] = 10] = "King";
+})(Values || (Values = {}));
+var newGame = new Game();
--- Blackjack_P1_KNG/BlackJackKG.ts
+++ Blackjack_P1_KNG/BlackJackKG.ts
@@ -1,3 +1,72 @@
-import readline from 'readline-sync';
-const move = readline.question("choose your move: (h)it, (s)tay.");
-console.log(move);
+//import readline from 'readline-sync';
+
+class Game {
+	cardHand: [number, number];
+	constructor(){
+		let deck = new Deck();
+		deck.shuffleDeck(deck.drawdeck)
+		console.log(deck.drawdeck);
+
+		//this.playerHand
+	}
+	startGame(): void {
+		//const move = readline.question("choose your move: (h)it, (s)tay.");
+		//console.log(move);
+	}
+}
+class Deck {
+	drawdeck: Array<number|string>;
+	constructor(){
+		this.drawdeck = new Array<number|string>;
+		let cardNum = 2;
+		for (let i = 0; i <= 52; i++){
+			if (i < 36){
+				this.drawdeck[i] = cardNum++;
+				if (cardNum == 10){
+					cardNum = 2
+				}
+			} 
+			else {
+				this.drawdeck[i++] = "Ace";
+				this.drawdeck[i++] = "Jack";
+				this.drawdeck[i++] = "Queen";
+				this.drawdeck[i] = "King";
+			}
+		}
+		console.log(this.drawdeck)
+	}
+	shuffleDeck(unshuffled: Array<number|string>): void {
+		for (let i = 0; i <= 52; i++){
+			let rndNum = Math.floor(Math.random() * 51)
+			let rndOne = unshuffled[i];
+			let rndTwo = unshuffled[rndNum];
+			unshuffled[rndNum] = rndOne;
+			unshuffled[i] = rndTwo;
+		} 
+	}
+	drawCard(): number {
+		//this.
+		return 0;
+	}
+}
+class Cards {
+	playername: string;
+	hand: [number, number];
+	constructor( playername: string){
+		this.playername = playername;
+		this.hand = [0,0];
+	}
+}
+enum Suits {
+	Hearts,
+	Diamonds,
+	Clubs,
+	Spades,
+}
+enum Values {
+	Ace = 11,
+	Jack = 10,
+	Queen = 10,
+	King = 10,
+}
+let newGame = new Game();
