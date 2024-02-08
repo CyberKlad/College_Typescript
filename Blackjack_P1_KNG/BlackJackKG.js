e548006ab484a55a738d4be408cab584680cfc6f
Korbin Gillette <korbingillette@Korbins-MacBook-Pro.local>
2024-02-07 18:41:31 -0800

    P1 first commit

---
+++ Blackjack_P1_KNG/BlackJackKG.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline_sync_1 = require("readline-sync");
+var move = readline_sync_1.default.question("choose your move: (h)it, (s)tay.");
+console.log(move);
---
+++ Blackjack_P1_KNG/BlackJackKG.ts
@@ -0,0 +1,3 @@
+import readline from 'readline-sync';
+const move = readline.question("choose your move: (h)it, (s)tay.");
+console.log(move);
